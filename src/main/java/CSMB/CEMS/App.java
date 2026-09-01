45f8723db9dbe1bb1285777b43575e701eb89018
kumbhambhargavi <bhargavikumbham@kmit.inye>
2026-09-01 22:40:49 +0530

    FS PROJET java

---
+++ src/main/java/CSMB/CEMS/App.java
@@ -0,0 +1,17 @@
+package CSMB.CEMS;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Campus Management System");
+
+        String studentName = "Rahul";
+        int studentId = 101;
+        String course = "Computer Science";
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Course: " + course);
+    }
+}
